b5c8749207b4c455986683343644f810bc39d698
divyajuluri <divyajuluri@gmail.com>
2022-12-08 10:58:35 +0530

    Adding Demo project

---
+++ src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func mainHandler() http.HandlerFunc {
+	now := time.Now()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World! ( build date: %s)", now)
+	})
+}
+
+func main() {
+	http.HandleFunc("/", mainHandler())
+	http.ListenAndServe(":8080", nil)
+}
